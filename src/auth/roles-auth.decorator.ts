a73113eef1caad81679e0422aec07f66f2e1edab
iamsixthousand <weare6000@gmail.com>
2022-01-07 06:22:20 +0300

    refactor: pretty-print 3

--- src/auth/roles-auth.decorator.ts
+++ src/auth/roles-auth.decorator.ts
@@ -2,4 +2,4 @@ import { SetMetadata } from '@nestjs/common';
 
 export const ROLE_KEY = 'role';
 
-export const Roles = (...roles: number[]) => SetMetadata(ROLE_KEY, roles);
+export const Roles = (...roles: number[]) => SetMetadata(ROLE_KEY, roles); // add custom decorator
